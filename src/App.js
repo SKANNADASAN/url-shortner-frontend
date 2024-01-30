8a889166668cf634c06b0abc0e2e97496418852e
AnithaG-96 <anithagk96@gmail.com>
2024-01-30 12:27:25 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
+import { Toaster } from 'react-hot-toast';
+import Background from './Background';
 import './App.css';
+import ChangePassword from './components/ChangePassword';
+import ForgetPassword from './components/ForgetPassword';
+import Register from './components/Register';
+import { Route, Routes } from 'react-router-dom';
+import Verification from './components/Verification';
+import { UserProvider } from './context/UserContext';
+import Login from "./components/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<UserProvider>
+  <Background/>
+  <Toaster/>
+  <Routes>
+  <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgetPassword />} />
+          <Route path="/verification" element={<Verification />} />
+          <Route path="/ChangePassword" element={<ChangePassword />} />
+          <Route path="/home" element={<Home/>} />
+  </Routes>
+</UserProvider>
+</>
   );
 }
 
